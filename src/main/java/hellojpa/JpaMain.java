c6a79f4d4a017e0f094eb4c07ad9ef575b424afa
Janghyeonsuk <vkdnjdjxor@naver.com>
2022-08-13 01:31:43 +0900

    Persistence Context code commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,22 +19,33 @@ public class JpaMain {
         try {
             /*
             //회원 등록
+            //비영속
             Member member = new Member();
             member.setId(2l);
             member.setName("HelloB");
-            em.persist(member); // insert 쿼리
-
+            //영속
+            em.persist(member); // insert 쿼리, 바로 DB에 저장되는 것x -> 1차 캐쉬에 저장
+            em.detached(member); // 회원 엔티티를 영속성 컨텍스트에서 분리
+            em.remove(member); // 실제 영구 저장을 데이터베이스에서 삭제
             //회원 조회
-            Member findMember = em.find(Member.class, 1l); //select 쿼리
+            Member findMember = em.find(Member.class, 1l); //select 쿼리, 만약 없다면 영속성 컨텍스트에 저장
             System.out.println("findMember.getId() = " + findMember.getId());
             System.out.println("findMember.getName() = " + findMember.getName());
 
             //회원 수정
-            findMember.setName("HelloJPA"); //update 쿼리
+            findMember.setName("HelloJPA"); //update 쿼리, 변경 감지
 
             //회원 삭제
             em.remove(findMember); //delete 쿼리
-            */
+
+            // 플러쉬
+            em.flush(); // 플러시는 영속성 컨텍스트 비우는것 x -> DB에 동기화
+
+            // 준영속
+            em.detach(member);
+            em.clear(); // EntityManager 안의 모든 영속성 컨텍스트를 모두 초기화
+            em.close(); // 영속성 컨텍스트 종료
+
 
             // 멤버 entity 대상으로 쿼리
             List<Member> result = em.createQuery("select m from Member as m ", Member.class)
@@ -44,9 +55,16 @@ public class JpaMain {
 
             for (Member member : result) {
                 System.out.println("member.getName() = " + member.getName());
-            }
+            }*/
 
-            tx.commit(); //정상적이면 commit
+            Member member = em.find(Member.class, 150l);
+            member.setName("AAAAA");
+
+
+
+            System.out.println("===============");
+
+            tx.commit(); //정상적이면 commit, DB에 저장되는 시점
         } catch (Exception e) {
             tx.rollback(); //예외 발생시 롤백
         } finally {
