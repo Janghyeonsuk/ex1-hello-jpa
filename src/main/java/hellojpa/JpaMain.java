08e146151d6b591a1cc547477e86a646370fb22c
Janghyeonsuk <vkdnjdjxor@naver.com>
2022-08-27 23:26:12 +0900

    Proxy code commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,11 @@
 package hellojpa;
 
+        import org.hibernate.Hibernate;
+
         import javax.persistence.EntityManager;
         import javax.persistence.EntityManagerFactory;
         import javax.persistence.EntityTransaction;
         import javax.persistence.Persistence;
-        import java.time.LocalDateTime;
-
 
 public class JpaMain {
 
@@ -23,14 +23,13 @@ public class JpaMain {
             //회원 등록
             //비영속
             Member member = new Member();
-            member.setId(2l);
-            member.setName("HelloB");
+            member.setId(1l);
+            member.setName("HelloA");
             //영속
             em.persist(member); // insert 쿼리, 바로 DB에 저장되는 것x -> 1차 캐쉬에 저장
             em.detached(member); // 회원 엔티티를 영속성 컨텍스트에서 분리
-            em.remove(member); // 실제 영구 저장을 데이터베이스에서 삭제
             //회원 조회
-            Member findMember = em.find(Member.class, 1l); //select 쿼리, 만약 없다면 영속성 컨텍스트에 저장
+            Member findMember = em.find(Member.class, 1l); //select 쿼리, 만약 없다면 DB 조회후 영속성 컨텍스트에 저장
             System.out.println("findMember.getId() = " + findMember.getId());
             System.out.println("findMember.getName() = " + findMember.getName());
 
@@ -47,7 +46,6 @@ public class JpaMain {
             em.detach(member);
             em.clear(); // EntityManager 안의 모든 영속성 컨텍스트를 모두 초기화
             em.close(); // 영속성 컨텍스트 종료
-
 
             // 멤버 entity 대상으로 쿼리
             List<Member> result = em.createQuery("select m from Member as m ", Member.class)
@@ -90,21 +88,56 @@ public class JpaMain {
                 System.out.println("m.getUsername() = " + m.getUsername());
             }
             System.out.println("=========================");
+
 */
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
-            em.persist(member);
+            em.flush();
+            em.clear();
+
+            // 프록시
+            Member reference = em.getReference(Member.class, member1.getId());
+            // 프록시는 실제 클래스를 상속받아서 만들어진다, 실제와 겉이 같아서 사용자는 실제인지 프록시인지 구분x
+            // 프록시 객체는 처음 사용할 때 한번만 초기화, 프록시 엔티티가 실제 엔티티로 바뀌는게 아닌 프록시 엔티티가 실제 엔티티에 접근 가능
+            System.out.println("reference = " + reference.getClass());
+            System.out.println("before reference.getId() = " + reference.getId());
+            System.out.println("reference.getUsername() = " + reference.getUsername());// target.getUsername();
+            System.out.println("after reference = " + reference.getClass()); // 그대로 프록시
+
+            // 프록시가 넘어올지, 실제가 올지 모르므로 타입 비교를 ==으로 하면 안되고 instance of 사용
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.getReference(Member.class, member1.getId());
+            System.out.println("m1==m2: " + (m1.getClass() == m2.getClass()));
+            System.out.println("m1 == member: " + (m1 instanceof Member));
+            System.out.println("m2 == member: " + (m2 instanceof Member));
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); //Proxy
+
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getClass());
+            // JPA 에서는 == 을 참으로 보장해주기 위해 둘을 맞춘다.
+            // 두번 getReference -> 둘다 프록시 반환
+            // 이미 영속성 컨텍스트에 있으면 프록시가 아닌 실제 엔티티 반환
+            // 프록시에서 한번을 조회하면 find 또한 프록시를 반환
+            System.out.println("(refMember == findMember) = " + (refMember == findMember));
+            // 프록시 인스턴스 초기화 여부 확인
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+
+            // 프록시 강제 초기화
+            Hibernate.initialize(refMember);
 
             tx.commit(); //정상적이면 commit, DB에 저장되는 시점
         } catch (Exception e) {
             tx.rollback(); //예외 발생시 롤백
+            e.printStackTrace();
         } finally {
             em.close(); //작업이 끝나면 EntityManager 종료 -> 내부적으로 DB connection 반환
         }
         emf.close(); //자원 반환
     }
+
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-
-import java.util.List;
 
 //JPA 관리하는 객체, DB 테이블과 매핑해서 사용
 @Entity
@@ -22,23 +19,6 @@ public class Member extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "TEAM_ID") //주인, 값을 생성 및 수정 가능, N 쪽이 무조건 연관관계 주인
     private Team team;
-
-    //일대일
-    //대상 테이블에 외래키가 있는 단방향일때는 지원x, 양방향은 가능
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    //다대다 -> 실무에서 사용x, 추가 데이터가 들어올수 있고, 쿼리가 이상 -> 다대일로 하고 엔티티하나를 생성
-    /*
-    @ManyToMany
-    @JoinTable(name = "MEMBER_PRODUCT")
-    private List<Product> products = new ArrayList<>();
-    */
-
-    //일대다로 변경
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -64,15 +44,6 @@ public class Member extends BaseEntity{
 //        this.teamId = teamId;
 //    }
 
-
-    public Locker getLocker() {
-        return locker;
-    }
-
-    public void setLocker(Locker locker) {
-        this.locker = locker;
-    }
-
     public Team getTeam() {
         return team;
     }
@@ -85,11 +56,6 @@ public class Member extends BaseEntity{
         // 그래서 Member team 세팅하면 Team 객체 콜렉션에도 member 추가된다.
     }
 
-    public List<MemberProduct> getMemberProducts() {
-        return memberProducts;
-    }
 
-    public void setMemberProducts(List<MemberProduct> memberProducts) {
-        this.memberProducts = memberProducts;
-    }
+
 }
